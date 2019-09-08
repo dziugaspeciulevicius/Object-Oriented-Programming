fb248114a5cdeb667acf0c763b7a006800fd4c1f
Džiugas Pečiulevičius <dziugaspeciulevicius@gmail.com>
2019-09-09 00:23:55 +0300

    Update Main.java

--- TASK1/src/com/company/Main.java
+++ TASK1/src/com/company/Main.java
@@ -8,15 +8,38 @@
 //EXTRA REQUIREMENT: Class Rectangle has to have single responsibility principle (5t)
 //---------------------------------------------------------------------------------------
 //SingleResponsibilityPrinciple: https://www.youtube.com/watch?v=bnKIDV-HsXA
-//Its a principle where one class is responsible no more than two different methods (functions)
+//Its a principle where one class is responsible for no more than two different methods (functions)
 //JAVA DOCUMENTATION BELOW FOR THIS EXERCISE:
-//https://docs.oracle.com/javase/7/docs/api/java/awt/Rectangle.html#isEmpty()
+//https://docs.oracle.com/javase/7/docs/api/java/awt/Rectangle.html#union(java.awt.Rectangle)
 
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
+    public static void promptEnterKey(){
+        System.out.println("Press ENTER key to continue...");
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+    }
+
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Hello, welcome to TASK1");
+        Main.promptEnterKey();
+
+        System.out.println("PLease enter the length of a rectangle: ");
+        int length = input.nextInt();
+        System.out.println("PLease enter the width of a rectangle:");
+        int width = input.nextInt();
+        System.out.println("Length: "+ length + "\nWidth: " + width);
+
+        System.out.println("Diagonal: " + Rectangle.getDiagonalLength(length, width));
+
+        System.out.println("The perimeter of a rectangle is: " + Rectangle.getPerimeter(length, width));
+        System.out.println("The square of a rectangle is: " + Rectangle.getArea(length, width));
 
     }
 }
