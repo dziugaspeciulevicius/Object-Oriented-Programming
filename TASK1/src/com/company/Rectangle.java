fd508c8c89a5b48d4589e50e86df17e2209d7f0e
Džiugas Pečiulevičius <dziugaspeciulevicius@gmail.com>
2019-09-08 22:41:40 +0300

    Update Rectangle.java

--- TASK1/src/com/company/Rectangle.java
+++ TASK1/src/com/company/Rectangle.java
@@ -1,18 +1,23 @@
 package com.company;
 
-import java.awt.geom.Rectangle2D;
-
 public class Rectangle {
 
     public int length;
     public int width;
-    public int x; //x coordinate of top left rectangle corner
-    public int y; //y coordinate of top left rectangle corner
+    public static int xCoordinate; //x coordinate of top left rectangle corner
+    public static int yCoordinate; //y coordinate of top left rectangle corner
 
+    public Rectangle(int length, int width, int x, int y) {
+        this.length = length;
+        this.width = width;
+        this.xCoordinate = x;
+        this.yCoordinate = y;
+    }
 
     public static int getPerimeter(int height, int width) {
         return (2 * height) + (2 * width);
     }
+
     public static int getSquare(int height, int width) {
         return height * width;
     }
