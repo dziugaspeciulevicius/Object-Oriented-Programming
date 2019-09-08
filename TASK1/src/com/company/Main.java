04f045606eb344f479d39eb54c4c89f3ada28d80
Džiugas Pečiulevičius <dziugaspeciulevicius@gmail.com>
2019-09-08 17:57:58 +0300

    Create Main.java

---
+++ TASK1/src/com/company/Main.java
@@ -0,0 +1,33 @@
+//Class Rectangle attributes: length, width, top left corner coordinates (x, y)
+//There has to be methods who return:
+//-Rectangle square size (2t)
+//-Rectangle perimeter (2t)
+//-Diagonal length (2t)
+//-Rectangle diagonals intersection coordinates (4t)
+//
+//EXTRA REQUIREMENT: Class Rectangle has to have single responsibility principle (5t)
+//SingleResponsibilityPrinciple: https://www.youtube.com/watch?v=bnKIDV-HsXA
+package com.company;
+
+import java.util.Scanner;
+
+public class Main{
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hello. Welcome to TASK1.");
+        System.out.println("Press \"ENTER\" to continue...");
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+
+        Rectangle rectangle = new Rectangle();
+        //type    objectName = operator   initializer(constructor)
+
+        System.out.println("Please enter the width and length of a rectangle:");
+
+
+
+
+    }
+}
