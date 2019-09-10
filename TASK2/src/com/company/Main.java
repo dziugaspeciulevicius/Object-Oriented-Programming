ab2c72a1af9ba58f1bea5cbbd5b4e5a7741bfaa5
Džiugas Pečiulevičius <dziugaspeciulevicius@gmail.com>
2019-09-10 22:26:39 +0300

    Update Main.java

--- TASK2/src/com/company/Main.java
+++ TASK2/src/com/company/Main.java
@@ -1,8 +1,18 @@
+//Write a program that calculates student grade average considering the number of credits.
+//MAIN REQUIREMENTS:
+//-Program has to let us insert student data (name, surname, group) and his lectures passed (subject name, evaluation, credit number)
+//-Program has to print full student information and calculated average value
+//
+//EXTRA REQUIREMENT: program architecture has to have single responsibility
+
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
 
     }
 }
