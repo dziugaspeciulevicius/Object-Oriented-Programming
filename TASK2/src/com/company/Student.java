04514dfcf4c1cd14be5d312a4c203513abf5ef17
Džiugas Pečiulevičius <dziugaspeciulevicius@gmail.com>
2019-09-10 22:26:42 +0300

    Create Student.java

---
+++ TASK2/src/com/company/Student.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Student {
+    public String name;
+    public String surname;
+    public String group;
+
+    public Student(String name, String surname, String group) {
+        this.name = name;
+        this.surname = surname;
+        this.group = group;
+    }
+}
