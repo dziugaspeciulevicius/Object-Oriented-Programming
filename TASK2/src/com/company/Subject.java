3e51c5fab54a59467ca7f91ba818176ae38d78d8
Džiugas Pečiulevičius <dziugaspeciulevicius@gmail.com>
2019-09-12 22:09:12 +0300

    Create Subject.java

---
+++ TASK2/src/com/company/Subject.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Subject {
+    public String subjectName;
+    public float evaluation;
+    public int amountOfCredits;
+
+    public Subject(String subjectName, float evaluation, int amountOfCredits) {
+        this.subjectName = subjectName;
+        this.evaluation = evaluation;
+        this.amountOfCredits = amountOfCredits;
+    }
+}
