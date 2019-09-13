4257babf4adbd6842fabd679f64008ee4f7284b6
Džiugas Pečiulevičius <dziugaspeciulevicius@gmail.com>
2019-09-14 01:55:29 +0300

    getters/setters

--- TASK2/src/com/company/Student.java
+++ TASK2/src/com/company/Student.java
@@ -1,13 +1,37 @@
 package com.company;
 
 public class Student {
-    public String name;
-    public String surname;
-    public String group;
+    private String name;
+    private String surname;
+    private String group;
 
-    public Student(String name, String surname, String group) {
+    /*public Student(String name, String surname, String group) {
         this.name = name;
         this.surname = surname;
         this.group = group;
+    }*/
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setName(String studentName) {
+        this.name = studentName;
+    }
+
+    public void setSurname(String studentSurname) {
+        this.surname = studentSurname;
+    }
+
+    public void setGroup(String studentGroup) {
+        this.group = studentGroup;
     }
 }
