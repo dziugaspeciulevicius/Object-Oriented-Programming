58950e6eafde109463e74df546b1c66dd59ab2f7
Džiugas Pečiulevičius <dziugaspeciulevicius@gmail.com>
2019-09-13 13:29:05 +0300

    Update Rectangle.java

--- TASK1/src/com/company/Rectangle.java
+++ TASK1/src/com/company/Rectangle.java
@@ -2,14 +2,31 @@ package com.company;
 
 public class Rectangle {
 
-    public float length;
-    public float width;
-    public float x, y; //x, y coordinates of top left rectangle corner
+    private float length;
+    private float width;
+    private float x, y; //x, y coordinates of top left rectangle corner
 
-    public Rectangle(float length, float width, float x, float y) {
+    private Rectangle(float length, float width, float x, float y) {
         this.length = length;
         this.width = width;
         this.x = x;
         this.y = y;
+    }
+
+    public static float getPerimeter(float length, float width) {
+        return (2 * length) + (2 * width);
+    }
+
+    public static float getArea(float length, float width) {
+        return length * width;
+    }
+
+    public static float getDiagonalLength(float length, float width) {
+        return (float) Math.sqrt(length * length + width * width);
+        //return (float) Math.sqrt(Math.pow(length, 2)+Math.pow(width, 2)); //typecast
+    }
+
+    public static void getDiagInterCoord(float length, float width) {
+        System.out.println("\nCoordinateX: " + (length/2) + "\nCoordinateY: " + (width/2));
     }
 }
