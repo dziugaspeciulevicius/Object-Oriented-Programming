52815229e94697ad7cdd23ed18277e9ce00e6e93
Džiugas Pečiulevičius <dziugaspeciulevicius@gmail.com>
2019-09-14 01:55:37 +0300

    getters/setters

--- TASK2/src/com/company/Subject.java
+++ TASK2/src/com/company/Subject.java
@@ -1,13 +1,37 @@
 package com.company;
 
 public class Subject {
-    public String subjectName;
-    public float evaluation;
-    public int amountOfCredits;
+    private String subjectName;
+    private float grade;
+    private int credits;
 
-    public Subject(String subjectName, float evaluation, int amountOfCredits) {
+    /*public Subject(String subjectName, float grade, int credits) {
         this.subjectName = subjectName;
-        this.evaluation = evaluation;
-        this.amountOfCredits = amountOfCredits;
+        this.grade = grade;
+        this.credits = credits;
+    }*/
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public float getGrade() {
+        return grade;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    public void setGrade(float subjectGrade) {
+        this.grade = subjectGrade;
+    }
+
+    public void setCredits(float subjectCredits) {
+        this.grade = subjectCredits;
     }
 }
