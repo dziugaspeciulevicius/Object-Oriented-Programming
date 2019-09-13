c19fada48d5b01afbbde29f515761f525ec8af1a
Džiugas Pečiulevičius <dziugaspeciulevicius@gmail.com>
2019-09-14 01:55:17 +0300

    added for loop for subjects

--- TASK2/src/com/company/Main.java
+++ TASK2/src/com/company/Main.java
@@ -14,15 +14,38 @@ public class Main {
 
         Scanner input = new Scanner(System.in);
 
+        Student student1 = new Student();
+
         System.out.println("Enter your name: ");
         String inputName = input.nextLine();
+        student1.setName(inputName);
         System.out.println("Enter your surname: ");
         String inputSurname = input.nextLine();
+        student1.setSurname(inputSurname);
         System.out.println("Enter your group name/number: ");
         String inputGroup = input.nextLine();
+        student1.setGroup(inputGroup);
+
+        System.out.println(student1.getName());
+        System.out.println(student1.getSurname());
+        System.out.println(student1.getGroup());
 
 
-        
+        System.out.println("Enter the amount of subjects you are taking this semester: ");
+        int inputSubjectNumber = input.nextInt();
+        for (int i = 0; i < inputSubjectNumber; i++){
+            System.out.println("HEllo");
+        }
+
+
+
+
+
+
+        //GPA FORMULA:
+        //(subject grade * subject credits) = subject grade and credit evaluation (X)
+        //(X + X + X + X + X + X) = total number of all subject evaluation (Y)
+        // (Y / number of credits in a semester) = GPA
 
         //System.out.println("Enter the number of subjects: ");
         //Student student1 = new Student(inputName, inputSurname, inputGroup);
