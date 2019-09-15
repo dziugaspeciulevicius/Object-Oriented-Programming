e9859fd1ffde47c51051900770b72bb36e6287e5
Džiugas Pečiulevičius <dziugaspeciulevicius@gmail.com>
2019-09-15 21:21:01 +0300

    Update Main.java

--- TASK2/src/com/company/Main.java
+++ TASK2/src/com/company/Main.java
@@ -27,54 +27,29 @@ public class Main {
         String inputGroup = input.nextLine();
         studentRecord.setStudentGroup(inputGroup);
 
-        String subjectName;
-        float subjectGrade;
-        int subjectCredits;
-
         System.out.println("Enter the amount of subjects you are taking this semester: ");
         int numberOfSubjects = input.nextInt();
-        //Record subject = new Record();
-        Record[] subject = new Record[numberOfSubjects];
-        for (int i = 0; i < numberOfSubjects; i++) {
-            subject[i] = new Record();
-
+        Record subjectRecord = new Record();
+        for (int i = 1; i <= numberOfSubjects; i++) {
             System.out.println("\nEnter the name of a subject #" + i + ":");
             String inputSubjectName = input.next();
-            subject[i].setSubjectName(inputSubjectName);
+            subjectRecord.setSubjectName(inputSubjectName);
             System.out.println("Enter the student grade of a subject #" + i + ":");
             float inputSubjectGrade = input.nextFloat();
-            subject[i].setSubjectGrade(inputSubjectGrade);
+            subjectRecord.setSubjectGrade(inputSubjectGrade);
             System.out.println("Enter the credit number of a subject #" + i + ":");
             int inputSubjectCredits = input.nextInt();
-            subject[i].setSubjectCredits(inputSubjectCredits);
+            subjectRecord.setSubjectCredits(inputSubjectCredits);
 
             System.out.println("\nSubject #" + i +
-                    "\nName: " + subject[i].getSubjectName() +
-                    "\nGrade: " + subject[i].getSubjectGrade() +
-                    "\nCredits: " + subject[i].getSubjectCredits());
-
-            /*
-            System.out.println("Enter the name of a subject #" + i + ":");
-            String inputSubjectName = input.next();
-            subject.setSubjectName(inputSubjectName);
-            System.out.println("Enter the student grade of a subject #" + i + ":");
-            float inputSubjectGrade = input.nextFloat();
-            subject.setSubjectGrade(inputSubjectGrade);
-            System.out.println("Enter the credit number of a subject #" + i + ":");
-            int inputSubjectCredits = input.nextInt();
-            subject.setSubjectCredits(inputSubjectCredits);
-
-            System.out.println("Subject #" + i +
-                    "\nName: " + subject.getSubjectName() +
-                    "\nGrade: " + subject.getSubjectGrade() +
-                    "\nCredits: " + subject.getSubjectCredits());
-            */
+                    "\nName: " + subjectRecord.getSubjectName() +
+                    "\nGrade: " + subjectRecord.getSubjectGrade() +
+                    "\nCredits: " + subjectRecord.getSubjectCredits());
         }
-        System.out.println("\nSubject #1 " + subject[0]);
 
-        /*System.out.println("Taken subjects: " + subject.getSubjectName() + subject.getSubjectGrade() +
-                subject.getSubjectCredits());
-        */
+        System.out.println("Taken subjects: " + subjectRecord.getSubjectName() + subjectRecord.getSubjectGrade() +
+                subjectRecord.getSubjectCredits());
+
 
         //GPA FORMULA:
         //(subject grade * subject credits) = subject grade and credit evaluation (X)
