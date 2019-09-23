50787fec3d51d4f1a4bd2ffb9d266c2c33efb1fd
Džiugas <dziugaspeciulevicius@gmail.com>
2019-09-23 23:14:33 +0300

    Update Student.java

--- TASK2/src/com/company/Student.java
+++ TASK2/src/com/company/Student.java
@@ -4,16 +4,14 @@ public class Student {
     private String studentName;
     private String studentSurname;
     private String studentGroup;
-    Subject subjectList;
+    Subject[] subjectList;
 
-    /*
-    public Student (String studentName, String studentSurname, String studentGroup, Subject subjectList) {
+    /*public Student (String studentName, String studentSurname, String studentGroup, Subject subjectList) {
             this.studentName = studentName;
             this.studentSurname = studentSurname;
             this.studentGroup = studentGroup;
             this.subjectList = subjectList;
     }*/
-
 
     public String getStudentName(){
         return studentName;
