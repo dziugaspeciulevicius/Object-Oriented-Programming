26b7f96b340150864315896d86f2eb1584cbe8b9
Džiugas <dziugaspeciulevicius@gmail.com>
2019-10-01 08:31:58 +0300

    Update Student.java

--- TASK2/src/com/company/Student.java
+++ TASK2/src/com/company/Student.java
@@ -1,61 +1,59 @@
 package com.company;
 
-public class Student extends Subject{
+public class Student {
     private String studentName;
     private String studentSurname;
     private String studentGroup;
     private int numberOfSubjects;
     Subject[] subjectList = new Subject[10];
 
-    public void setStudentName(String studentName){
+    public void setStudentName(String studentName) {
         this.studentName = studentName;
     }
 
-    public void setStudentSurname(String studentSurname){
+    public void setStudentSurname(String studentSurname) {
         this.studentSurname = studentSurname;
     }
 
-    public void setStudentGroup(String studentGroup){
+    public void setStudentGroup(String studentGroup) {
         this.studentGroup = studentGroup;
     }
 
-    public void setNumberOfSubjects(int numberOfSubjects){
+    public void setNumberOfSubjects(int numberOfSubjects) {
         this.numberOfSubjects = numberOfSubjects;
     }
 
-    public String getStudentName(){
+    public String getStudentName() {
         return studentName;
     }
 
-    public String getStudentSurname(){
+    public String getStudentSurname() {
         return studentSurname;
     }
 
-    public String getStudentGroup(){
+    public String getStudentGroup() {
         return studentGroup;
     }
 
-    public float getGPA(){
+    public float getGPA() {
         float x = 0;
         float y = 0;
 
+        /*
+        //---THIS LOOP WORKS FINE---//
         for (int i = 1; i <= numberOfSubjects; i++) {
             x = x + subjectList[i].getSubjectCredits() * subjectList[i].getSubjectGrade();
             y = y + subjectList[i].getSubjectCredits();
         }
         return x / y;
-    }
-    /*
-    for (numberOfSubjects:subjectList) {
-        x = x + subjectList[i].getSubjectCredits() * subjectList[i].getSubjectGrade();
-        y = y + subjectList[i].getSubjectCredits();
-    }
-    */
+        */
 
-    /*
-    GPA FORMULA:
-    (subject grade * subject credits) = subject grade and credit evaluation (X)
-    (X + X + X + X + X + X) = total number of all subject evaluation (Y)
-    (Y / number of credits in a semester) = GPA
-    */
+        //---THIS LOOP DOES NOT WORK---//
+
+        for (Subject s : subjectList) {
+            x = x + s.getSubjectCredits() * s.getSubjectGrade();
+            y = y + s.getSubjectCredits();
+        }
+        return x / y;
+    }
 }
