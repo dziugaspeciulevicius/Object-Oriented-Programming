b42ae5690203caeebbe25c1f4743aa19ec14c513
Džiugas <dziugaspeciulevicius@gmail.com>
2019-10-10 14:49:18 +0300

    Table View

---
+++ JavaFXPractice/[15]MAKING MENUS/src/sample/Main.java
@@ -0,0 +1,32 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    Stage window;
+    BorderPane layout;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        window = primaryStage;
+        primaryStage.setTitle("Menu");
+
+
+
+
+        layout = new BorderPane();
+        primaryStage.setScene(new Scene(layout, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
