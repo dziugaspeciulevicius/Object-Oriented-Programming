b64b2f0586a9126fc8c13a1c2e4781a8b21f826c
Džiugas <dziugaspeciulevicius@gmail.com>
2019-11-14 00:05:56 +0200

    menu window

--- TASK4/src/sample/Main.java
+++ TASK4/src/sample/Main.java
@@ -10,9 +10,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("MenuWindow.fxml"));
+        primaryStage.setTitle("Menu");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
