00291928d242b5065bfb5cfbb6eaa05b14ef5db6
Džiugas <dziugaspeciulevicius@gmail.com>
2019-11-19 23:45:30 +0200

    put all info into db via sqlite browser

--- TASK4/src/sample/Driver.java
+++ TASK4/src/sample/Driver.java
@@ -5,15 +5,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
+import java.io.*;
 import java.sql.*;
 
 public class Driver extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("MenuWindow.fxml"));
         primaryStage.setTitle("Menu");
         primaryStage.setScene(new Scene(root));
@@ -21,48 +19,44 @@ public class Driver extends Application {
         primaryStage.show();
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception{
         launch(args);
-        //createTable();
-        //insert();
     }
 
-    private static Connection connection(String database) throws SQLException {
+    private static Connection connection(String dish) {
+        Connection conn = null;
+        try {
+            String url = ("jdbc:sqlite:dish.db");
+            conn = DriverManager.getConnection(url);
+            //Class.forName("org.sqlite.JDBC");
+            //conn = DriverManager.getConnection("jdbc:sqlite:dish.db");
+            System.out.println("Connected!");
+        } catch (Exception exc) {
+        System.out.println(exc.getMessage());
+            System.out.println("Failed to connect");
+        }
+        return conn;
+    }
+}
+
+    /*
+    private static Connection connection(String dish) throws SQLException {
         Connection conn = null;
         try {
             Class.forName("org.sqlite.JDBC"); //calling JDBC class to connect
-            conn = DriverManager.getConnection("jdbc:sqlite:"+database+".db");
+            conn = DriverManager.getConnection("jdbc:sqlite:dish.db");
         } catch (Exception e){
-            System.out.println(e);
+            System.out.println(e.getMessage());
         } finally {
             if (conn.isClosed() == false) {
                 conn.close();
             }
+            return conn;
         }
+    */
 
-        //return conn;
-    }
-
-    private static void createTable(){
-        Connection conn = connection("dish");
-        Statement stmt;
-        try {
-            stmt = conn.createStatement();
-            String sql =
-                    "CREATE TABLE dish" +
-                    "(dish_name VARCHAR(15) PRIMARY KEY NOT NULL," +
-                    "dish_price DOUBLE NOT NULL," +
-                    "dish_description VARCHAR(255) NOT NULL," + //VARCHAR can max 255, TEXT a lot more
-                    "dish_picture BLOB NOT NULL)";
-            stmt.executeUpdate(sql);
-            stmt.close();
-            conn.close();
-        } catch (Exception e){
-            System.out.println(e);
-        }
-    }
-
-    private static void insert(){
+/*
+        private static void insert(){
         try {
             FileInputStream fis;    //fis - file input stream
             int num_rows = 0;       //see how many rows effected
@@ -94,4 +88,58 @@ public class Driver extends Application {
             System.out.println(e);
         }
     }
-}
+*/
+
+    /*
+        Connection conn = null;
+        PreparedStatement ps = null;
+        InputStream is = null;
+        try {
+            //Class.forName("org.sqlite.JDBC");
+            conn = DriverManager.getConnection("jdbc:sqlite:dish.db");
+            System.out.println("Connection successful");
+            //ps = conn.prepareCall("INSERT INTO dish VALUES(?,?,?,?)");
+            ps = conn.prepareCall("INSERT INTO dish(dish_name, dish_price, dish_description, dish_picture) VALUES (?,?,?,?)");
+            ps.setString(1, "Pizza"); //1st ? = dish name
+            ps.setDouble(2, 10.99); //2nd ? = dish price
+            ps.setString(3, "Savory dish of Italian origin, consisting of round, flattened base of leavened " +
+                    "wheat-based dough topped with tomatoes, cheese, olives and some salami. Baked at " +
+                    "a high temperature, traditionally in a wood-fired oven. "); //3rd ? = dish description
+            is = new FileInputStream(new File("D:\\MY FILES\\Studies\\3 SEMESTER\\Object-Oriented-Programming\\TASK4\\src\\Images\\pizza.png"));
+            ps.setBinaryStream(4, is);
+        //} catch (ClassNotFoundException e) {
+        //    e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if(is != null) is.close();
+                if(ps != null) ps.close();
+                if(conn != null) conn.close();
+            } catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+        }*/
+
+/*
+    private static void createTable(){
+        Connection conn = connection("dish");
+        Statement stmt;
+        try {
+            stmt = conn.createStatement();
+            String sql =
+                    "CREATE TABLE dish" +
+                    "(dish_name VARCHAR(15) PRIMARY KEY NOT NULL," +
+                    "dish_price DOUBLE NOT NULL," +
+                    "dish_description VARCHAR(255) NOT NULL," + //VARCHAR can max 255, TEXT a lot more
+                    "dish_picture BLOB NOT NULL)";
+            stmt.executeUpdate(sql);
+            stmt.close();
+            conn.close();
+        } catch (Exception e){
+            System.out.println(e);
+        }
+    }
+    */
